0ea6eff53a8b7bccbfee8b7b910249e3bfd5a44c
burning_git <974201360@qq.com>
2015-05-29 15:34:13 +0800

    Initial Commit

---
+++ BRQRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BRQRCode
+//
+//  Created by gitBurning on 15/5/29.
+//  Copyright (c) 2015年 BR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
